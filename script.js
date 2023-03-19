7b71ec8f147afe6478b34991390faea247204e06
Jordan Bédard-Lessard <34403496+JordanBedard@users.noreply.github.com>
2023-03-18 21:05:37 -0400

    Navigation Menu

--- script.js
+++ script.js
@@ -16,18 +16,26 @@ function createHeaderDOM() {
 }
 createHeaderDOM();
 
-// My Collection Cards
+// Fetch JSON Files
+function fetchdataCollection() {
+    fetch("./dataCollection.json")
+        .then(response => response.json())
+        .then(data => updateCards(data.vinyls));
+}
 
-// Fetch JSON File
-fetch("./data.json")
-    .then(response => response.json())
-    .then(data => updateCards(data.vinyls));
+function fetchdataWl() {
+    fetch("./dataWhisList.json")
+        .then(response => response.json())
+        .then(data => updateCards(data.wishlist));
+}
 
-// Function Update Cards
-function updateCards(cards) {
-    const cardsContainer = document.querySelector(".cardscontainer");
+// Function Update Cards Collection
+function updateCards(data) {
+    const cardsCompleteLayout = document.querySelector(".cardsLayout");
 
-    cards.forEach((cardResults) => {
+    cardsCompleteLayout.innerHTML = '';
+
+    data.forEach((result) => {
         // Create Div for each Card
         const cardsComplete = document.createElement("div");
         cardsComplete.classList.add("cards");
@@ -36,29 +44,72 @@ function updateCards(cards) {
         cardImage.classList.add("cardimage");
         // Insert image in Div
         const imageSource = document.createElement("img");
-        imageSource.src = `./img/albums/${cardResults.albumImg}`; imageSource.alt = cardResults.imgAlt;
+        imageSource.src = `./img/albums/${result.albumImg}`; imageSource.alt = result.imgAlt;
         imageSource.classList.add("cardimage");
         // Create Div for content
         const content = document.createElement("div");
         content.classList.add("content");
         // Author
         const cardAuthor = document.createElement('p');
-        cardAuthor.textContent = `Author : ${cardResults.author}`;
+        cardAuthor.textContent = `Author : ${result.author}`;
         // Album
         const cardAlbum = document.createElement('p');
-        cardAlbum.textContent = `Album : ${cardResults.albumName}`;
+        cardAlbum.textContent = `Album : ${result.albumName}`;
         // Year
         const cardYear = document.createElement('p');
-        cardYear.textContent = `Year : ${cardResults.year}`;
+        cardYear.textContent = `Year : ${result.year}`;
         // Genre
         const cardGenre = document.createElement('p');
-        cardGenre.textContent = `Genre : ${cardResults.genre}`;
+        cardGenre.textContent = `Genre : ${result.genre}`;
         // Append 
         content.append(cardAuthor, cardAlbum, cardYear, cardGenre);
         cardImage.append(imageSource);
         cardsComplete.append(cardImage, content);
-        cardsContainer.append(cardsComplete);
+        cardsCompleteLayout.append(cardsComplete);
     });
 }
 
-updateCards();
+// Using Menu Items
+
+document.querySelector(".btnCollection").addEventListener("click", loadingContentCollection);
+
+function loadingContentCollection() {
+    fetchdataCollection();
+}
+
+document.querySelector(".btnWishlist").addEventListener("click", loadingContentWhislist);
+
+function loadingContentWhislist() {
+    fetchdataWl();
+}
+
+// Google chart
+/*
+src = "https://www.gstatic.com/charts/loader.js"
+
+function drawChart() {
+
+var data = google.visualization.arrayToDataTable([
+  ['Genre', 'Number'],
+  ['Classic Rock', 55],
+  ['Progressive Rock', 49],
+  ['Heavy Rock', 44],
+  ['Alternative', 24],
+  ['Classical', 15],
+  ['Jazz', 15],
+  ['Funk', 15],
+  ['Pop', 15],
+  ['Latin', 15],
+  ['Hip Hop', 15],
+  ['Electronic', 15],
+]);
+
+var options = {
+  title: 'World Wide Wine Production'
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+chart.draw(data, options);
+}
+
+*/
